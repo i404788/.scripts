77f34cdc19ba2d8ea1c0798a8f87e532022f2269
Casper <casper@devdroplets.com>
2020-10-13 22:37:45 +0000

    Add docs

---
+++ README.md
@@ -0,0 +1,9 @@
+# Usage
+
+* Clone in `~/` or `/opt/`
+* `cd .scripts`
+* Run `./init.sh`
+* Add below to your `~/.profile` or other shell init scripts
+```
+export PATH=$PATH:$(pwd)
+```
---
+++ init.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+
+assert_command_exists () {
+	command -v $1 >/dev/null 2>&1 || { echo "I require $1 but it's not installed.  Aborting." >&2; exit 1; }
+}
+
+assert_command_exists node
+assert_command_exists yarn
+assert_command_exists git
+assert_command_exists pandoc
+assert_command_exists pdflatex
+
+echo "Found all dependencies..."
+
+git submodule update --init --recursive
+
+install_yarn_deps() {
+	cd $1
+	yarn install
+	cd ..
+}
+
+yarn
+install_yarn_deps asciimathml-pandocfilter
+install_yarn_deps lev2
+install_yarn_deps mermaid-filter
